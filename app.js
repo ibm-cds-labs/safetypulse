90c0498edb3f126a5d86d90e76bcea4e647fd7c1
Raj Singh <rrsingh@us.ibm.com>
2017-03-20 08:42:02 -0400

    upgrades 2017

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 /*jshint node:true*/
 
 // app.js
-// This file contains the server side JavaScript code for your application.
-// This sample application uses cron to run this app every day
-// console.log('TESTVAR: ' + process.env.TESTVAR);
 var express = require('express');
 var app = express();
 var http = require('http');
 var request = require('request');
 
-//-- Cloudant settings
-// var password = process.env.CLOUDANTPASSWORD || process.argv[4];
-// var dbuser = process.env.CLOUDANTUSER || process.argv[3] || 'opendata';
-// var dbname = process.env.CLOUDANTDB || process.argv[2] || 'crimes';
 var dbservice = process.env.DBSERVICE || process.argv[2] || 'https://opendata.mybluemix.net/crimes';
-//-- end Cloudant
 
 //-- express web server settings
 app.set('title', 'SafetyPulse');
@@ -24,12 +16,10 @@ app.use(express.static(__dirname + '/public'));
 app.get('/querycrimes', queryCrimes);
 //-- end express settings
 
-// The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
-var host = (process.env.VCAP_APP_HOST || 'localhost');
-// The port on the DEA for communication with the application:
+var host = (process.env.HOST || 'localhost');
 var port = (process.env.PORT || 3000);
-// Start server
-app.listen(port, host);
+
+app.listen(port, host); // Start server
 
 function queryCrimes(req, res) {
     radius = req.query.radius ? parseInt(req.query.radius) : 500;
